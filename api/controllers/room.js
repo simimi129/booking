69d24a99e0bd665e45aa36eb682f3bf137b45b21
simimi129 <simimi129@gmail.com>
2022-11-29 17:52:41 +0100

    adding room model, routes

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -37,8 +37,16 @@ export const updateRoom = async (req, res, next) => {
 };
 
 export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
   try {
     await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (error) {
+      next(error);
+    }
     res.status(200).json("Room deleted.");
   } catch (error) {
     next(error);
