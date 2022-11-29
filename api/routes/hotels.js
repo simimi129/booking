c0d95f1e38aa656667d0933610e2cfb2c03fd615
simimi129 <simimi129@gmail.com>
2022-11-29 17:32:36 +0100

    adding verify

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,12 +6,13 @@ import {
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utilities/verify.js";
 
 const router = express.Router();
 
-router.post("/", createHotel);
-router.put("/:id", updateHotel);
-router.delete("/:id", deleteHotel);
+router.post("/", verifyAdmin, createHotel);
+router.put("/:id", verifyAdmin, updateHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 router.get("/:id", getHotel);
 router.get("/", getHotels);
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,27 +5,13 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js";
-import {
-  verifyAdmin,
-  verifyToken,
-  verifyUser,
-} from "../utilities/verifyToken.js";
+import { verifyAdmin, verifyUser } from "../utilities/verify.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.send("User logged in.");
-});
-router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-  res.send("User logged in and can delete.");
-});
-router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
-  res.send("Admin logged in and can delete.");
-});
-
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
-router.get("/", getUsers);
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
