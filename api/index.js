6bc8cf3efe3ce89aab827d2b699222f446d35539
simimi129 <simimi129@gmail.com>
2022-11-29 13:30:54 +0100

    setting up db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongodb.");
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.listen(5000, () => {
+  connect();
   console.log("Connected to backend.");
 });
