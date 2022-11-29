840827fbc5d2959a792fbddce6f47cf897753f66
simimi129 <simimi129@gmail.com>
2022-11-29 15:13:56 +0100

    adding register route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("auth");
-});
-
-router.get("/register", (req, res) => {
-  res.send("register");
-});
+router.post("/register", register);
 
 export default router;
