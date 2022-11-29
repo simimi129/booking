7024e8e647c2c6f91ad6157d48fb33e6fc441fe7
simimi129 <simimi129@gmail.com>
2022-11-29 13:54:07 +0100

    adding hotel routes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,17 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
