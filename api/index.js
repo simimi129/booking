4e16b8d08e6c78a3240ca4bac1cc3ac7cf8fa0ff
simimi129 <simimi129@gmail.com>
2022-11-29 17:04:13 +0100

    adding login route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -18,6 +19,7 @@ async function connect() {
   }
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/auth", authRoute);
